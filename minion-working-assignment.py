05e0222f79cc870d997d13f6962bf27f913080e0
ParisW97 <114314201+ParisW97@users.noreply.github.com>
2022-12-13 23:10:15 -0500

    removed commented out code

--- minion-working-assignment.py
+++ minion-working-assignment.py
@@ -1,11 +1,5 @@
 from collections import Counter
 
-# # list of int named 'data'
-# data = [1,2,2,3,3,4,4,4,4]
-# # int 'n' used for allowed instances of any number in 'data
-# n = 1
-
-
 def main():
     # list of int named 'data'
     data = [1,2,2,3,3,4,4,4,4]
@@ -22,31 +16,6 @@ def main():
     # print address of list
     print(id(data))
 
-
-
-
-
-# print(data)
-
-# counts = Counter(data)
-
-# # print dictionary
-# print(counts)
-
-# print keys of dictionary
-# for k, elem in counts.items():
-#     print(k, elem)
-#     if elem <= n:
-#         # allowable instances of entry
-#         continue
-#     # exceeds limit - remove specific entry from list
-#     for i in data[:]:
-#         if i == k:
-#             data.remove(i)
-#     print(data)
-# print(data)
-# print key values of dictionary
-
 def solution(data:list,n:int):
     # create dict from list of employee IDs
     dataDict = Counter(data)
@@ -57,9 +26,6 @@ def solution(data:list,n:int):
             continue
         # remove employee ID from list
         removeID(data,n,key,keyValue,dataDict)
-        # for id in data[:]:
-        #     if id == key:
-        #         data.remove(key)
 
 def removeID(data:list,n:int,key:int,keyValue:int,dataDict:dict):
     for id in data[:]:
